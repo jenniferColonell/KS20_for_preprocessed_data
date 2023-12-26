fb27020f03ef6ea50e80a016ef179133701d1517
unknown <labadmin@colonellj-lw2.hhmi.org>
2023-12-26 16:38:54 -0500

    option to use cpu for svd in template compression

--- mainLoop/runTemplates.m
+++ mainLoop/runTemplates.m
@@ -83,16 +83,28 @@ rez.U_a = zeros(Nchan* Nrank, nKeep, Nfilt, 'single');
 rez.U_b = zeros(Nbatches, nKeep, Nfilt, 'single');
 for j = 1:Nfilt
     % do this for every template separately
+    % fprintf('compressing templates for unit: %d\n', j);
     WA = reshape(rez.WA(:, j, :, :), [], Nbatches);
     WA = gpuArray(WA); % svd on the GPU was faster for this, but the Python randomized CPU version might be faster still
-    [A, B, C] = svdecon(WA);
+    try
+        [A, B, C] = svdecon(WA);
+    catch
+        fprintf('error in gpu svdecon of WA for template %d, using cpu\n',j);
+        [A, B, C] = svd(WA,'econ');
+    end
     % W_a times W_b results in a reconstruction of the time components
     rez.W_a(:,:,j) = gather(A(:, 1:nKeep) * B(1:nKeep, 1:nKeep));
     rez.W_b(:,:,j) = gather(C(:, 1:nKeep));
     
     UA = reshape(rez.UA(:, j, :, :), [], Nbatches);
     UA = gpuArray(UA);
-    [A, B, C] = svdecon(UA);
+    try
+        [A, B, C] = svdecon(UA);
+    catch
+        fprintf('error in gpu svdecon of UA for template %d, using cpu\n',j);
+        [A, B, C] = svd(UA,'econ');
+    end
+    
     % U_a times U_b results in a reconstruction of the time components
     rez.U_a(:,:,j) = gather(A(:, 1:nKeep) * B(1:nKeep, 1:nKeep));
     rez.U_b(:,:,j) = gather(C(:, 1:nKeep));
